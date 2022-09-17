6fda9f2482b20e935dde8654e714850b021d241b
ovsmar <88561951+ovsmar@users.noreply.github.com>
2022-09-17 17:14:27 +0200

    Add files via upload

--- DiscordBot/app.py
+++ DiscordBot/app.py
@@ -39,22 +39,16 @@ async def on_ready():
      
     scheduler.start()
 
-# @bot.command()
-# @commands.has_permissions(administrator=True)
-# async def close(ctx):
-#     response_close = f"Tu m'as tué... 😡 \n Désactivation du bot dans 5 secondes..."
-#     msgC= await ctx.send(response_close)
-#     await asyncio.sleep(5)
-#     await ctx.message.delete()
-#     await msgC.delete() 
-#     await bot.close()
-
 @bot.command()
-@commands.is_owner()
-async def remoteshutdown(ctx):
-    await ctx.send("Shutting down")
-    # Shuts down the bot
-    await bot.logout()
+@commands.has_permissions(administrator=True)
+async def close(ctx):
+    response_close = f"Tu m'as tué... 😡 \n Désactivation du bot dans 5 secondes..."
+    msgC= await ctx.send(response_close)
+    await asyncio.sleep(5)
+    await ctx.message.delete()
+    await msgC.delete() 
+    await bot.close()
+
      
 @bot.command()
 @commands.has_permissions(administrator=True)
