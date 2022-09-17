99baceb81eec4aba402f468503dd68863e325007
ovsmar <88561951+ovsmar@users.noreply.github.com>
2022-09-17 19:53:31 +0200

    Update app.py

--- DiscordBot/app.py
+++ DiscordBot/app.py
@@ -35,7 +35,7 @@ async def on_ready():
     scheduler.add_job(weekend, CronTrigger(hour="16", minute="55", second="0",day_of_week ="fri"))
     
 #TEST
-    scheduler.add_job(SendMessage, CronTrigger(hour="19, 22", minute="50", second="0",day_of_week ="sat" )) 
+    scheduler.add_job(SendMessage, CronTrigger(hour="19, 22", minute="55", second="0",day_of_week ="sat" )) 
      
     scheduler.start()
 
