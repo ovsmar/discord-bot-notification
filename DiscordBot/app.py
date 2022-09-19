d56b18ee51c57a5800e9174306bda4a2df34bf9b
ovsmar <88561951+ovsmar@users.noreply.github.com>
2022-09-19 09:41:13 +0200

    Update app.py

--- DiscordBot/app.py
+++ DiscordBot/app.py
@@ -31,11 +31,11 @@ async def on_ready():
     print("Ready")
     scheduler = AsyncIOScheduler()
 
-    scheduler.add_job(SendMessage, CronTrigger(hour="10, 15", minute="30", second="0",day_of_week ="mon,tue,wed,thu,fri"))
+    scheduler.add_job(SendMessage, CronTrigger(hour="10, 15", minute="0", second="0",day_of_week ="mon,tue,wed,thu,fri"))
     scheduler.add_job(weekend, CronTrigger(hour="16", minute="55", second="0",day_of_week ="fri"))
     
 #TEST
-    scheduler.add_job(SendMessage, CronTrigger(hour="08", minute="00", second="0",day_of_week ="sun" )) 
+    
      
     scheduler.start()
 
